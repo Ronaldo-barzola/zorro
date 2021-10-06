72b97541c9c0fd4283612ef1b067d077b8678176
RonaldoBarzola <barzolaronaldog@gmail.com>
2021-10-05 23:56:47 -0500

    Inicio de proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule } from '@angular/core';
+import { Routes, RouterModule, Router } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+
+
+
+const routes: Routes = [
+  {
+    path: "", component: MenuComponent
+  }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
